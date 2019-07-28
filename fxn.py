f35a75137689b246902df386ae48f005615611d6
Talon <talonpollard@gmail.com>
2019-07-28 14:12:04 -0700

    Update consume + measure methods

--- fxn.py
+++ fxn.py
@@ -10,24 +10,25 @@ import pprint
 # API Keys file
 from api_keys import key
 
+
 limitNutrient = {
     "Biotin": ("mcg", 300), "Folate/Folic Acid": ("mcg", 400),
     "Niacin": ("mg", 20), "Pantothenic Acid": ("mg", 10),
     "Riboflavin": ("mg", 1.7), "Thiamin": ("mg", 1.5),
     "Vitamin A, IU": ("IU", 5000), "Vitamin B-6": ("mg", 2),
     "Vitamin B-12": ("mcg", 6), "Vitamin C, total ascorbic acid": ("mg", 60),
-    "Vitamin D": ("IU", 400), "Vitamin E": ("IU", 30),
-    "Vitamin K": ("mcg", 80), "Calcium": ("mg", 1000),
+    "Vitamin D": ("IU", 400), "Vitamin E (alpha-tocopherol)": ("IU", 30),
+    "Vitamin K": ("mcg", 80), "Calcium, Ca": ("mg", 1000),
     "Chloride": ("mg", 3400), "Chromium": ("mcg", 120),
     "Copper": ("mg", 2), "Iodine": ("mcg", 150),
-    "Iron": ("mg", 18), "Magnesium": ("mg", 400),
+    "Iron, Fe": ("mg", 18), "Magnesium, Mg": ("mg", 400),
     "Manganese": ("mg", 2), "Molybdenum": ("mcg", 75),
-    "Phosphorus": ("mg", 1000), "Potassium": ("mg", 3500),
-    "Selenium": ("mcg", 70), "Sodium": ("mg", 2400),
-    "Zinc": ("mg", 15), "Total lipid (fat)": ("g", 65, 80),
+    "Phosphorus, P": ("mg", 1000), "Potassium, K": ("mg", 3500),
+    "Selenium": ("mcg", 70), "Sodium, Na": ("mg", 2400),
+    "Zinc, Zn": ("mg", 15), "Total lipid (fat)": ("g", 65, 80),
     "Fatty acids, total saturated": ("g", 20, 25),
     "Carbohydrate, by difference": ("g", 300, 375),
-    "Fiber, total dietary": ("g", 25, 30), "Sugar": ("g", 50),
+    "Fiber, total dietary": ("g", 25, 30), "Sugars, total": ("g", 50),
     "Cholesterol": ("mg", 300), "Caffeine": ("mg", 400)
 }
 
@@ -37,21 +38,23 @@ intakeNutrient = {
     "Riboflavin": [0, 0, 0], "Thiamin": [0, 0, 0],
     "Vitamin A, IU": [0, 0, 0], "Vitamin B-6": [0, 0, 0],
     "Vitamin B-12": [0, 0, 0], "Vitamin C, total ascorbic acid": [0, 0, 0],
-    "Vitamin D": [0, 0, 0], "Vitamin E": [0, 0, 0],
-    "Vitamin K": [0, 0, 0], "Calcium": [0, 0, 0],
+    "Vitamin D": [0, 0, 0], "Vitamin E (alpha-tocopherol)": [0, 0, 0],
+    "Vitamin K": [0, 0, 0], "Calcium, Ca": [0, 0, 0],
     "Chloride": [0, 0, 0], "Chromium": [0, 0, 0],
     "Copper": [0, 0, 0], "Iodine": [0, 0, 0],
-    "Iron": [0, 0, 0], "Magnesium": [0, 0, 0],
+    "Iron, Fe": [0, 0, 0], "Magnesium, Mg": [0, 0, 0],
     "Manganese": [0, 0, 0], "Molybdenum": [0, 0, 0],
-    "Phosphorus": [0, 0, 0], "Potassium": [0, 0, 0],
-    "Selenium": [0, 0, 0], "Sodium": [0, 0, 0],
-    "Zinc": [0, 0, 0], "Total lipid (fat)": [0, 0, 0],
+    "Phosphorus, P": [0, 0, 0], "Potassium, K": [0, 0, 0],
+    "Selenium": [0, 0, 0], "Sodium, Na": [0, 0, 0],
+    "Zinc, Zn": [0, 0, 0], "Total lipid (fat)": [0, 0, 0],
     "Fatty acids, total saturated": [0, 0, 0],
     "Carbohydrate, by difference": [0, 0, 0],
-    "Fiber, total dietary": [0, 0, 0], "Sugar": [0, 0, 0],
-    "Cholesterol": [0, 0, 0], "Caffeine": [0, 0, 0]
+    "Fiber, total dietary": [0, 0, 0], "Sugars, total": [0, 0, 0],
+    "Cholesterol": [0, 0, 0], "Caffeine": [0, 0, 0],
+    "Fatty acids, total monounsaturated": [0, 0, 0],
+    "Fatty acids, total polyunsaturated": [0, 0, 0],
+    "Fatty acids, total trans": [0, 0, 0]
 }
-
 
 def checkItem(food):
     barInfo = decode(Image.open(food))
@@ -78,7 +81,7 @@ def checkItem(food):
     return name
 
 
-def measure(food, quantity):
+def measure(food, quantity = 1, show = True):
     uri = "https://api.nal.usda.gov/ndb/search/?"
     query = "format=json&q=" + checkItem(food) + "&max=25&api_key=" + key["product"]
     urlFood = uri + query
@@ -104,29 +107,51 @@ def measure(food, quantity):
     filter = data["foods"][0]["food"]["nutrients"]
     # print(json.dumps(filter, indent=4, sort_keys=True))
 
-    eqv = str(filter[0]["measures"][0]["eqv"])
+    eqv = str(filter[0]["measures"][0]["eqv"] * quantity)
     unit = filter[0]["unit"]
 
-    print("Showing results for one serving of " + eqv + unit)
-    print("--------------------------")
+    nutrition = {}
+
+    if show:
+        print("Showing results for", str(quantity), "serving(s) (" + eqv + unit + ")")
+        print("--------------------------")
+
     for i in filter:
         name = i["name"]
-        value = str(i["measures"][0]["value"])
-        # eqv = str(i["measures"][0]["eqv"])
-        # unit = i["unit"]
+        value = float(i["measures"][0]["value"]) * quantity
+
+        nutrition[name] = [unit, value]
         
-        result = name + ": " + value + unit
-        print(result)
+        if show:
+            result = name + ": " + str(value) + unit
+            print(result)
 
-    # return smth
+    return nutrition
 
 
-def consume(food, quantity):
-    nutrients = measure(food, quantity)
+def consume(food, quantity = 1):
+    nutrition = measure(food, show = False)
+    
+    for i in nutrition:
+        if i != "Water" and i != "Energy" and i != "Protein":
+            if nutrition[i][0] == "g" or nutrition[i][0] == "IU":
+                intakeNutrient[i][0] += nutrition[i][1] * quantity
+            elif nutrition[i][0] == "mg":
+                intakeNutrient[i][1] += nutrition[i][1] * quantity
+            elif nutrition[i][0] == "mcg":
+                intakeNutrient[i][2] += nutrition[i][1] * quantity
+
 
 
 def viewStats():
-    pass
+    for i in intakeNutrient:
+        intakeNutrient[i] = [round(num, 3) for num in intakeNutrient[i]]
+        # print(i + ": " + str([round(num, 3) for num in intakeNutrient[i]]))
+    
+    print(intakeNutrient)
+
+
+
 
 
 # if vitamin in limitNutrient:
@@ -134,5 +159,9 @@ def viewStats():
 
 food = 'photos/quakerbarcode.png'
 
-# checkItem(food)
-# measure(food, 1)
+checkItem(food)
+# measure(food)
+consume(food, 3)
+# consume(food)
+# consume(food)
+viewStats()
