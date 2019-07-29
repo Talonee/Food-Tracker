73eed47c52e1099f3b38a3126bc447c6c750725d
Talon <talonpollard@gmail.com>
2019-07-28 18:33:29 -0700

    Complete view, consume, measure functions

--- fxn.py
+++ fxn.py
@@ -10,56 +10,88 @@ import pprint
 # API Keys file
 from api_keys import key
 
-
 limitNutrient = {
-    "Biotin": ("mcg", 300), "Folate/Folic Acid": ("mcg", 400),
-    "Niacin": ("mg", 20), "Pantothenic Acid": ("mg", 10),
-    "Riboflavin": ("mg", 1.7), "Thiamin": ("mg", 1.5),
-    "Vitamin A, IU": ("IU", 5000), "Vitamin B-6": ("mg", 2),
-    "Vitamin B-12": ("mcg", 6), "Vitamin C, total ascorbic acid": ("mg", 60),
-    "Vitamin D": ("IU", 400), "Vitamin E (alpha-tocopherol)": ("IU", 30),
-    "Vitamin K": ("mcg", 80), "Calcium, Ca": ("mg", 1000),
-    "Chloride": ("mg", 3400), "Chromium": ("mcg", 120),
-    "Copper": ("mg", 2), "Iodine": ("mcg", 150),
-    "Iron, Fe": ("mg", 18), "Magnesium, Mg": ("mg", 400),
-    "Manganese": ("mg", 2), "Molybdenum": ("mcg", 75),
-    "Phosphorus, P": ("mg", 1000), "Potassium, K": ("mg", 3500),
-    "Selenium": ("mcg", 70), "Sodium, Na": ("mg", 2400),
-    "Zinc, Zn": ("mg", 15), "Total lipid (fat)": ("g", 65, 80),
-    "Fatty acids, total saturated": ("g", 20, 25),
+    "Biotin": ("mcg", 300),
+    "Folate/Folic Acid": ("mcg", 400),
+    "Niacin": ("mg", 20),
+    "Pantothenic Acid": ("mg", 10),
+    "Riboflavin": ("mg", 1.7),
+    "Thiamin": ("mg", 1.5),
+    "Vitamin A, IU": ("IU", 5000),
+    "Vitamin B-6": ("mg", 2),
+    "Vitamin B-12": ("mcg", 6),
+    "Vitamin C, total ascorbic acid": ("mg", 60),
+    "Vitamin D": ("IU", 400),
+    "Vitamin E (alpha-tocopherol)": ("IU", 30),
+    "Vitamin K": ("mcg", 80),
+    "Calcium, Ca": ("mg", 1000),
+    "Chloride": ("mg", 3400),
+    "Chromium": ("mcg", 120),
+    "Copper": ("mg", 2),
+    "Iodine": ("mcg", 150),
+    "Iron, Fe": ("mg", 18),
+    "Magnesium, Mg": ("mg", 400),
+    "Manganese": ("mg", 2),
+    "Molybdenum": ("mcg", 75),
+    "Phosphorus, P": ("mg", 1000),
+    "Potassium, K": ("mg", 3500),
+    "Selenium": ("mcg", 70),
+    "Sodium, Na": ("mg", 2400, "less"),
+    "Zinc, Zn": ("mg", 15),
+    "Total lipid (fat)": ("g", 65, 80, "less"),
+    "Fatty acids, total saturated": ("g", 20, 25, "less"),
     "Carbohydrate, by difference": ("g", 300, 375),
-    "Fiber, total dietary": ("g", 25, 30), "Sugars, total": ("g", 50),
-    "Cholesterol": ("mg", 300), "Caffeine": ("mg", 400)
+    "Fiber, total dietary": ("g", 25, 30),
+    "Sugars, total": ("g", 50, "less"),
+    "Cholesterol": ("mg", 300, "less"),
+    "Caffeine": ("mg", 400)
 }
 
 intakeNutrient = {
-    "Biotin": [0, 0, 0], "Folate/Folic Acid": [0, 0, 0],
-    "Niacin": [0, 0, 0], "Pantothenic Acid": [0, 0, 0],
-    "Riboflavin": [0, 0, 0], "Thiamin": [0, 0, 0],
-    "Vitamin A, IU": [0, 0, 0], "Vitamin B-6": [0, 0, 0],
-    "Vitamin B-12": [0, 0, 0], "Vitamin C, total ascorbic acid": [0, 0, 0],
-    "Vitamin D": [0, 0, 0], "Vitamin E (alpha-tocopherol)": [0, 0, 0],
-    "Vitamin K": [0, 0, 0], "Calcium, Ca": [0, 0, 0],
-    "Chloride": [0, 0, 0], "Chromium": [0, 0, 0],
-    "Copper": [0, 0, 0], "Iodine": [0, 0, 0],
-    "Iron, Fe": [0, 0, 0], "Magnesium, Mg": [0, 0, 0],
-    "Manganese": [0, 0, 0], "Molybdenum": [0, 0, 0],
-    "Phosphorus, P": [0, 0, 0], "Potassium, K": [0, 0, 0],
-    "Selenium": [0, 0, 0], "Sodium, Na": [0, 0, 0],
-    "Zinc, Zn": [0, 0, 0], "Total lipid (fat)": [0, 0, 0],
+    "Biotin": [0, 0, 0],
+    "Folate/Folic Acid": [0, 0, 0],
+    "Niacin": [0, 0, 0],
+    "Pantothenic Acid": [0, 0, 0],
+    "Riboflavin": [0, 0, 0],
+    "Thiamin": [0, 0, 0],
+    "Vitamin A, IU": [0, 0, 0],
+    "Vitamin B-6": [0, 0, 0],
+    "Vitamin B-12": [0, 0, 0],
+    "Vitamin C, total ascorbic acid": [0, 0, 0],
+    "Vitamin D": [0, 0, 0],
+    "Vitamin E (alpha-tocopherol)": [0, 0, 0],
+    "Vitamin K": [0, 0, 0],
+    "Calcium, Ca": [0, 0, 0],
+    "Chloride": [0, 0, 0],
+    "Chromium": [0, 0, 0],
+    "Copper": [0, 0, 0],
+    "Iodine": [0, 0, 0],
+    "Iron, Fe": [0, 0, 0],
+    "Magnesium, Mg": [0, 0, 0],
+    "Manganese": [0, 0, 0],
+    "Molybdenum": [0, 0, 0],
+    "Phosphorus, P": [0, 0, 0],
+    "Potassium, K": [0, 0, 0],
+    "Selenium": [0, 0, 0],
+    "Sodium, Na": [0, 0, 0],
+    "Zinc, Zn": [0, 0, 0],
+    "Total lipid (fat)": [0, 0, 0],
     "Fatty acids, total saturated": [0, 0, 0],
     "Carbohydrate, by difference": [0, 0, 0],
-    "Fiber, total dietary": [0, 0, 0], "Sugars, total": [0, 0, 0],
-    "Cholesterol": [0, 0, 0], "Caffeine": [0, 0, 0],
+    "Fiber, total dietary": [0, 0, 0],
+    "Sugars, total": [0, 0, 0],
+    "Cholesterol": [0, 0, 0],
+    "Caffeine": [0, 0, 0],
     "Fatty acids, total monounsaturated": [0, 0, 0],
     "Fatty acids, total polyunsaturated": [0, 0, 0],
     "Fatty acids, total trans": [0, 0, 0]
 }
 
+
 def checkItem(food):
     barInfo = decode(Image.open(food))
     barCode = str(barInfo[0].data)[2:-1]
-    
+
     uri = "https://api.barcodelookup.com/v2/products?"
     query = "barcode=" + barCode + "&formatted=y&key=" + key["barcode"]
     urlBar = uri + query
@@ -81,9 +113,10 @@ def checkItem(food):
     return name
 
 
-def measure(food, quantity = 1, show = True):
+def measure(food, quantity=1, show=True):
     uri = "https://api.nal.usda.gov/ndb/search/?"
-    query = "format=json&q=" + checkItem(food) + "&max=25&api_key=" + key["product"]
+    query = "format=json&q=" + checkItem(
+        food) + "&max=25&api_key=" + key["product"]
     urlFood = uri + query
 
     with urllib.request.urlopen(urlFood) as url:
@@ -108,29 +141,36 @@ def measure(food, quantity = 1, show = True):
     # print(json.dumps(filter, indent=4, sort_keys=True))
 
     eqv = str(filter[0]["measures"][0]["eqv"] * quantity)
-    unit = filter[0]["unit"]
+    eunit = filter[0]["measures"][0]["eunit"]
 
     nutrition = {}
 
     if show:
-        print("Showing results for", str(quantity), "serving(s) (" + eqv + unit + ")")
+        print("Showing results for", str(quantity),
+              "serving(s) (" + eqv + eunit + ")")
         print("--------------------------")
 
     for i in filter:
         name = i["name"]
         value = float(i["measures"][0]["value"]) * quantity
+        unit = i["unit"]
+
 
         nutrition[name] = [unit, value]
-        
+
         if show:
             result = name + ": " + str(value) + unit
             print(result)
 
+    if show:
+        print()
+        print()
+
     return nutrition
 
 
-def consume(food, quantity = 1):
-    nutrition = measure(food, show = False)
+def consume(food, quantity=1):
+    nutrition = measure(food, show=False)
     
     for i in nutrition:
         if i != "Water" and i != "Energy" and i != "Protein":
@@ -142,26 +182,58 @@ def consume(food, quantity = 1):
                 intakeNutrient[i][2] += nutrition[i][1] * quantity
 
 
-
 def viewStats():
     for i in intakeNutrient:
         intakeNutrient[i] = [round(num, 3) for num in intakeNutrient[i]]
         # print(i + ": " + str([round(num, 3) for num in intakeNutrient[i]]))
-    
-    print(intakeNutrient)
 
+    # print(intakeNutrient)
 
+    for i in limitNutrient:
+        total = 0
+        unit = limitNutrient[i][0]
 
+        if unit == "g" or unit == "IU":
+            total = intakeNutrient[i][0] + intakeNutrient[i][
+                1] / 1000 + intakeNutrient[i][2] / 1000000
+        elif unit == "mg":
+            total = intakeNutrient[i][0] * 1000 + intakeNutrient[i][
+                1] + intakeNutrient[i][2] / 1000
+        elif unit == "mg":
+            total = intakeNutrient[i][0] * 1000000 + intakeNutrient[i][
+                1] / 1000 + intakeNutrient[i][2]
 
+        total = round(total, 3)
 
-# if vitamin in limitNutrient:
-#     raise NameError
+        if total != 0:
+            diff = round(limitNutrient[i][1] - total, 3)
+
+            if limitNutrient[i][-1] == "less": # consume less
+                print(i + ": (" + str(total) + " / " + str(limitNutrient[i][1]) + unit + ")")
+                
+                if diff <= limitNutrient[i][1] * .2: # difference < 20%
+                    print("   Caution: " + str(diff) + unit,
+                        "away from upper limit.")
+                elif diff > limitNutrient[i][1] * .2:
+                    print("   Safe: " + str(diff) + unit,
+                        "away from upper limit.")
+                elif diff == 0:
+                    print("   S T O P : " + str(diff) + unit,
+                        "away from upper limit.")
+            else: 
+                print(i + ": (" + str(total) + " / " + str(limitNutrient[i][1]) + unit + ")")
+                
+                if diff >= 0:
+                    print("   Safe: " + str(diff) + unit,
+                        "away from lower limit.")
+                elif diff < 0:
+                    print("   Caution: " + str(abs(diff)) + unit,
+                        "above the lower limit.")
+
 
 food = 'photos/quakerbarcode.png'
 
 checkItem(food)
 # measure(food)
-consume(food, 3)
-# consume(food)
-# consume(food)
+consume(food, 4)
 viewStats()
